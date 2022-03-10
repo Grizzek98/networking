011a51fa745e54b765524426c0ee839dd49e8e77
Benjamin Anderson <benjaminanderson98@live.com>
2022-03-10 10:55:53 -0700

    update

--- client.py
+++ client.py
@@ -1,10 +1,48 @@
 import socket
-import pickle
+import select
+import errno
+import sys
 
-HEADERSIZE = 10
+ 
+HEADER_LENGTH = 10
+
+IP = "127.0.0.1"
+PORT = 1234
+
+my_username = input("Username: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
+client_socket.send(username_header + username)
+
+while True:
+    message = input(f"{my_username} > ")
+
+    if message:
+        message = message.encode('utf-8')
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
+        client_socket.send(message_header + message)
+
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER_LENGTH)
+            if not len(username_header):
+                print("connection closed by the server")
+                sys.exit()
+            username_length = int(username_length.decode)
+        
+    except:
+        pass
 
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+
+'''s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 1234))
 
 while True:
@@ -30,3 +68,4 @@ while True:
             full_msg = b''
 
     print(full_msg)
+'''
